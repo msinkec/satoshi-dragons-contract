9b6892dd7fd39f52605a3dfed7900e0a1355a3ea
msinkec <sinkec.mihael@gmail.com>
2024-04-26 17:16:13 +0200

    initial commit

---
+++ src/contracts/satoshiDragonsContract.ts
@@ -0,0 +1,252 @@
+import { Ordinal, OrdinalNFT } from 'scrypt-ord'
+import {
+    method,
+    prop,
+    hash256,
+    assert,
+    ByteString,
+    SigHash,
+    slice,
+    toByteString,
+    Utils,
+    PubKey,
+    pubKey2Addr,
+    Sig,
+    len,
+    int2ByteString,
+} from 'scrypt-ts'
+import { BlockHeader, Blockchain, MerkleProof } from 'scrypt-ts-lib'
+
+export class SatoshiDragonsContract extends OrdinalNFT {
+    @prop()
+    static readonly TARGET_DIFFICULTY: bigint =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935n
+
+    @prop(true)
+    ownerPubKey: PubKey
+
+    @prop(true)
+    power: bigint
+
+    // Battle related info:
+    @prop(true)
+    oppositeOwnerPubKey: PubKey
+
+    @prop(true)
+    oppositeDragonPower: bigint
+
+    @prop(true)
+    isChallengingPlayer: boolean
+
+    @prop(true)
+    isBattling: boolean
+
+    constructor(ownerPubKey: PubKey) {
+        super()
+        this.init(...arguments)
+        this.ownerPubKey = ownerPubKey
+        this.power = 1n
+        this.oppositeOwnerPubKey = PubKey(
+            toByteString(
+                '000000000000000000000000000000000000000000000000000000000000000000'
+            )
+        )
+        this.oppositeDragonPower = 0n
+        this.isChallengingPlayer = false
+        this.isBattling = false
+    }
+
+    /**
+     * This public method is called when P1 challenges P2.
+     * The call must be made to both instances in order to count as valid.
+     * In the opposite case, e.g. if a player tries to feed their dragon with
+     * dummy instances, it will be deemed burned by game clients.
+     * @param isChallengingPlayer
+     * @param oppositeDragonPower
+     */
+    @method(SigHash.ANYONECANPAY_SINGLE)
+    public challenge(
+        isChallengingPlayer: boolean,
+        oppositeOwnerPubKey: PubKey,
+        oppositeDragonPower: bigint,
+        sig: Sig
+    ) {
+        assert(this.checkSig(sig, this.ownerPubKey))
+
+        this.isChallengingPlayer = isChallengingPlayer
+        this.oppositeOwnerPubKey = oppositeOwnerPubKey
+        this.oppositeDragonPower = oppositeDragonPower
+        this.isBattling = true
+
+        const outputs = this.buildStateOutputNFT()
+        assert(
+            this.ctx.hashOutputs == hash256(outputs),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method()
+    public battleExec(merkleProof: MerkleProof, bh: BlockHeader) {
+        // Check prevouts are from same tx.
+        this.checkBattlePrevouts()
+
+        // Get entropy from BH.
+        //assert(
+        //    Blockchain.isValidBlockHeader(
+        //        bh, SatoshiDragonsContract.TARGET_DIFFICULTY
+        //    ),
+        //    'invalid block header'
+        //)
+        //assert(
+        //    Blockchain.txInBlock(
+        //        Sha256(this.ctx.utxo.outpoint.txid), bh, merkleProof, 32
+        //    ),
+        //    'invalid merkle proof'
+        //)
+        const rand = Blockchain.blockHeaderHashAsInt(bh)
+
+        // Calculate battle results.
+        const pP1 = 100n * this.power
+        const pP2 = 100n * this.oppositeDragonPower
+        const pick = rand % (pP1 + pP2)
+        const won = this.isChallengingPlayer ? pick < pP1 : pick >= pP2
+
+        // Store some info for use further down below...
+        const isChallengingPlayerCurrent = this.isChallengingPlayer
+        const oppositeOwnerPubKeyCurrent = this.oppositeOwnerPubKey
+
+        // Update states and reset battle info.
+        if (won) {
+            this.power += 1n
+
+            this.oppositeDragonPower = 0n
+            this.isChallengingPlayer = false
+            this.isBattling = false
+            this.oppositeOwnerPubKey = PubKey(
+                toByteString(
+                    '0000000000000000000000000000000000000000000000000000000000000000'
+                )
+            )
+        } else {
+            this.oppositeDragonPower += 1n
+        }
+
+        // Outputs used in case of a win of the respective player:
+        const stateOutputThisPlayer = this.buildStateOutputNFT()
+        const stateOutputOppositePlayer = Utils.buildOutput(
+            this.updateStateScriptPropsManually(
+                Ordinal.removeInsciption(this.getStateScript()),
+                this.oppositeOwnerPubKey,
+                PubKey(
+                    toByteString(
+                        '0000000000000000000000000000000000000000000000000000000000000000'
+                    )
+                ),
+                this.oppositeDragonPower,
+                0n,
+                false,
+                false
+            ),
+            1n
+        )
+
+        // Construct and enforce subsequent outputs.
+        let outputs = toByteString('')
+        if (isChallengingPlayerCurrent) {
+            if (won) {
+                outputs += stateOutputThisPlayer // OK
+                outputs += Utils.buildAddressOutput(
+                    pubKey2Addr(oppositeOwnerPubKeyCurrent),
+                    1n
+                ) // OK
+            } else {
+                outputs += Utils.buildAddressOutput(
+                    pubKey2Addr(this.ownerPubKey),
+                    1n
+                )
+                outputs += stateOutputOppositePlayer
+            }
+        } else {
+            if (won) {
+                outputs += Utils.buildAddressOutput(
+                    pubKey2Addr(oppositeOwnerPubKeyCurrent),
+                    1n
+                )
+                outputs += stateOutputThisPlayer
+            } else {
+                outputs += stateOutputOppositePlayer
+                outputs += Utils.buildAddressOutput(
+                    pubKey2Addr(this.ownerPubKey),
+                    1n
+                )
+            }
+        }
+
+        assert(
+            this.ctx.hashOutputs == hash256(outputs),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method()
+    checkBattlePrevouts(): void {
+        if (this.isChallengingPlayer) {
+            assert(
+                this.ctx.utxo.outpoint.outputIndex == 0n,
+                'wrong output index'
+            )
+
+            assert(
+                this.ctx.utxo.outpoint.txid == slice(this.prevouts, 36n, 68n),
+                'second input wrong txid'
+            )
+            assert(
+                toByteString('01000000') == slice(this.prevouts, 68n, 72n),
+                'second input wrong output index'
+            )
+        } else {
+            assert(
+                this.ctx.utxo.outpoint.outputIndex == 1n,
+                'wrong output index'
+            )
+
+            assert(
+                this.ctx.utxo.outpoint.txid == slice(this.prevouts, 0n, 32n),
+                'first input wrong txid'
+            )
+            assert(
+                toByteString('00000000') == slice(this.prevouts, 32n, 36n),
+                'first input wrong output index'
+            )
+        }
+    }
+
+    @method()
+    updateStateScriptPropsManually(
+        script: ByteString,
+        ownerPubKey: PubKey,
+        oppositeOwnerPubKey: PubKey,
+        power: bigint,
+        oppositeDragonPower: bigint,
+        isChallengingPlayer: boolean,
+        isBattling: boolean
+    ): ByteString {
+        const lenTotal = len(script)
+        const lenOpReturn = 80n
+        const prefixScript = slice(script, 0n, lenTotal - lenOpReturn)
+        return (
+            prefixScript +
+            toByteString('0121') +
+            ownerPubKey +
+            toByteString('01') +
+            int2ByteString(power, 1n) + // TODO: Make larger possible
+            toByteString('20') +
+            oppositeOwnerPubKey +
+            toByteString('01') +
+            int2ByteString(oppositeDragonPower, 1n) + // TODO: Make larger possible
+            (isChallengingPlayer ? toByteString('01') : toByteString('00')) +
+            (isBattling ? toByteString('01') : toByteString('00')) +
+            toByteString('4b00000000')
+        )
+    }
+}
---
+++ tests/satoshiDragonsContract.test.ts
@@ -0,0 +1,315 @@
+import { use } from 'chai'
+import { SatoshiDragonsContract } from '../src/contracts/satoshiDragonsContract'
+import { getDefaultSigner } from './utils/txHelper'
+import {
+    MethodCallOptions,
+    PubKey,
+    Sha256,
+    SmartContract,
+    StatefulNext,
+    Utils,
+    bsv,
+    fill,
+    findSig,
+    pubKey2Addr,
+    reverseByteString,
+    toByteString,
+} from 'scrypt-ts'
+import chaiAsPromised from 'chai-as-promised'
+import { myPublicKey } from './utils/privateKey'
+import { BlockHeader, Blockchain, MerklePath, MerkleProof } from 'scrypt-ts-lib'
+use(chaiAsPromised)
+
+describe('Test SmartContract `SatoshiDragonsContract`', () => {
+    const dragon0Signer = getDefaultSigner()
+    const dragon1Signer = getDefaultSigner()
+
+    let dragon0: SatoshiDragonsContract
+    let dragon1: SatoshiDragonsContract
+
+    let deployTxDragon0: bsv.Transaction
+    let deployTxDragon1: bsv.Transaction
+
+    before(async () => {
+        await SatoshiDragonsContract.loadArtifact()
+
+        dragon0 = new SatoshiDragonsContract(PubKey(myPublicKey.toByteString()))
+        await dragon0.connect(dragon0Signer)
+
+        dragon1 = new SatoshiDragonsContract(PubKey(myPublicKey.toByteString()))
+        await dragon1.connect(dragon1Signer)
+
+        // Deploy dragon #0
+        deployTxDragon0 = await dragon0.inscribeText('Dragon #0')
+        console.log(`Deployed dragon #0: ${deployTxDragon0.id}`)
+
+        // Deploy dragon #1
+        deployTxDragon1 = await dragon1.inscribeText('Dragon #1')
+        console.log(`Deployed dragon #1: ${deployTxDragon1.id}`)
+    })
+
+    it('challenge', async () => {
+        // Dragon #0 challenges dragon #1 by constructing a PSBT
+        const dragon0Next = dragon0.next()
+        dragon0Next.isChallengingPlayer = true
+        dragon0Next.oppositeOwnerPubKey = dragon1.ownerPubKey
+        dragon0Next.oppositeDragonPower = dragon1.power
+        dragon0Next.isBattling = true
+
+        const dragon1Next = dragon1.next()
+        dragon1Next.isChallengingPlayer = false
+        dragon1Next.oppositeOwnerPubKey = dragon0.ownerPubKey
+        dragon1Next.oppositeDragonPower = dragon0.power
+        dragon1Next.isBattling = true
+
+        dragon0.bindTxBuilder(
+            'challenge',
+            async (
+                current: SatoshiDragonsContract,
+                options: MethodCallOptions<SatoshiDragonsContract>
+            ) => {
+                const unsignedTx: bsv.Transaction = new bsv.Transaction()
+                    .addInput(current.buildContractInput())
+                    .addInput(
+                        new bsv.Transaction.Input({
+                            prevTxId: deployTxDragon1.id,
+                            outputIndex: 1,
+                            script: bsv.Script.fromHex('00'.repeat(1000)), // Dummy script
+                        }),
+                        deployTxDragon1.outputs[0].script,
+                        1
+                    )
+                    .addOutput(
+                        new bsv.Transaction.Output({
+                            script: dragon0Next.lockingScript,
+                            satoshis: 1,
+                        })
+                    )
+                    .addOutput(
+                        new bsv.Transaction.Output({
+                            script: dragon1Next.lockingScript,
+                            satoshis: 1,
+                        })
+                    )
+
+                // build change output
+                if (options.changeAddress) {
+                    unsignedTx.change(options.changeAddress)
+                }
+
+                return Promise.resolve({
+                    tx: unsignedTx,
+                    atInputIndex: 0,
+                    nexts: [
+                        {
+                            instance: dragon0Next,
+                            atOutputIndex: 0,
+                            balance: 1,
+                        },
+                        {
+                            instance: dragon1Next,
+                            atOutputIndex: 1,
+                            balance: 1,
+                        },
+                    ],
+                })
+            }
+        )
+
+        const dragon0SignerAddr = await dragon0Signer.getDefaultAddress()
+        const partialTx = await dragon0.methods.challenge(
+            true,
+            dragon1.ownerPubKey,
+            dragon1.power,
+            (sigResps) => findSig(sigResps, dragon0SignerAddr),
+            {
+                multiContractCall: true,
+                changeAddress: dragon0SignerAddr,
+                pubKeyOrAddrToSign: dragon0SignerAddr,
+            } as MethodCallOptions<SatoshiDragonsContract>
+        )
+
+        // Dragon #1 accepts challange by signing PSBT
+        dragon1.bindTxBuilder(
+            'challenge',
+            async (
+                current: SatoshiDragonsContract,
+                options: MethodCallOptions<SatoshiDragonsContract>
+            ) => {
+                if (options.partialContractTx) {
+                    const unSignedTx = options.partialContractTx.tx
+                    unSignedTx.inputs[1] = current.buildContractInput()
+
+                    return Promise.resolve({
+                        tx: unSignedTx,
+                        atInputIndex: 1,
+                        nexts: [],
+                    })
+                }
+
+                throw new Error('no partialContractTx found')
+            }
+        )
+
+        dragon1.from = {
+            txId: deployTxDragon1.id,
+            outputIndex: 0,
+            script: deployTxDragon1.outputs[0].script.toHex(),
+            satoshis: 1,
+        }
+
+        const dragon1SignerAddr = await dragon1Signer.getDefaultAddress()
+        const finalTx = await dragon1.methods.challenge(
+            false,
+            dragon0.ownerPubKey,
+            dragon0.power,
+            (sigResps) => findSig(sigResps, dragon1SignerAddr),
+            {
+                multiContractCall: true,
+                partialContractTx: partialTx,
+                pubKeyOrAddrToSign: dragon1SignerAddr,
+            } as MethodCallOptions<SatoshiDragonsContract>
+        )
+
+        const { tx: callTx, nexts } = await SmartContract.multiContractCall(
+            finalTx,
+            dragon1Signer
+        )
+
+        dragon0 = nexts[0].instance
+        dragon1 = nexts[1].instance
+
+        console.log('Battle challange accepted: ', callTx.id)
+    })
+
+    it('battle', async () => {
+        // Once the challenge is accepted and the tx has a confirmation,
+        // the battle can be executed by anyone.
+
+        // Mock BH:
+        const bh: BlockHeader = {
+            version: reverseByteString(toByteString('20200000'), 4n),
+            prevBlockHash: Sha256(
+                reverseByteString(
+                    toByteString(
+                        '00000000000000000331a174d2a4256e648767cb14be8be161c6ebaa0b8b8222'
+                    ),
+                    32n
+                )
+            ),
+            merkleRoot: Sha256(
+                reverseByteString(
+                    toByteString(
+                        // challangeTx.id TODO: switch back to txid
+                        '0000000000000000000000000000000000000000000000000000000000000000'
+                    ),
+                    32n
+                )
+            ),
+            time: 1690000000n,
+            bits: reverseByteString(toByteString('00000000'), 4n),
+            nonce: 0x00000000n,
+        }
+
+        const merkleProof: MerkleProof = fill(
+            // Fill with blanks:
+            {
+                hash: Sha256('00'.repeat(32)),
+                pos: MerklePath.INVALID_NODE,
+            },
+            32
+        )
+
+        const battleExecTxBuilder = async (
+            current: SatoshiDragonsContract,
+            options: MethodCallOptions<SatoshiDragonsContract>,
+            merkleProof: MerkleProof,
+            bh: BlockHeader
+        ) => {
+            const unsignedTx: bsv.Transaction = options.partialContractTx
+                ? options.partialContractTx.tx
+                : new bsv.Transaction()
+
+            unsignedTx.addInput(current.buildContractInput())
+
+            // Calculate battle results.
+            const rand = Blockchain.blockHeaderHashAsInt(bh)
+            const pP1 = 100n * current.power
+            const pP2 = 100n * current.oppositeDragonPower
+            const pick = rand % (pP1 + pP2)
+            const won = current.isChallengingPlayer ? pick < pP1 : pick >= pP2
+
+            const nexts: StatefulNext<SatoshiDragonsContract>[] = []
+            if (won) {
+                const next = current.next()
+                next.power += 1n
+
+                next.oppositeDragonPower = 0n
+                next.isChallengingPlayer = false
+                next.isBattling = false
+                next.oppositeOwnerPubKey = PubKey(
+                    toByteString(
+                        '0000000000000000000000000000000000000000000000000000000000000000'
+                    )
+                )
+
+                unsignedTx.addOutput(
+                    new bsv.Transaction.Output({
+                        script: next.lockingScript,
+                        satoshis: 1,
+                    })
+                )
+                nexts.push({
+                    instance: next,
+                    balance: 1,
+                    atOutputIndex: 0,
+                })
+            } else {
+                unsignedTx.addOutput(
+                    new bsv.Transaction.Output({
+                        script: bsv.Script.fromHex(
+                            Utils.buildPublicKeyHashScript(
+                                pubKey2Addr(current.ownerPubKey)
+                            )
+                        ),
+                        satoshis: 1,
+                    })
+                )
+            }
+
+            //// build change output
+            //if (options.changeAddress) {
+            //    unsignedTx.change(options.changeAddress)
+            //}
+
+            return Promise.resolve({
+                tx: unsignedTx,
+                atInputIndex: current.isChallengingPlayer ? 0 : 1,
+                nexts,
+            })
+        }
+
+        dragon0.bindTxBuilder('battleExec', battleExecTxBuilder)
+
+        const partialTx = await dragon0.methods.battleExec(merkleProof, bh, {
+            multiContractCall: true,
+        } as MethodCallOptions<SatoshiDragonsContract>)
+
+        dragon1.bindTxBuilder('battleExec', battleExecTxBuilder)
+
+        const finalTx = await dragon1.methods.battleExec(merkleProof, bh, {
+            multiContractCall: true,
+            partialContractTx: partialTx,
+        } as MethodCallOptions<SatoshiDragonsContract>)
+
+        const { tx: callTx } = await SmartContract.multiContractCall(
+            finalTx,
+            dragon1Signer
+            //{
+            //    autoPayFee: false
+            //} as MultiContractCallOptions
+        )
+
+        console.log('Battle executed: ', callTx.id)
+    })
+})
